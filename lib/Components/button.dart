12a0c6144108d0946af73805a2d0c5e1cddd2cc9
SolomonB127 <solomonakinsulire@gmail.com>
2024-09-14 00:41:46 +0100

    feat: Add NextButton component for flexible navigation
    - Implemented NextButton as a reusable component with customizable tap action and child widget.

--- lib/Components/button.dart
+++ lib/Components/button.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 
 class NextButton extends StatelessWidget {
-  const NextButton({super.key});
+  final void Function()? onTap;
+  final Widget child;
+  const NextButton({super.key, required this.onTap, required this.child});
 
   @override
   Widget build(BuildContext context) {
-    return Container(
-      
+    return GestureDetector(
+      onTap: onTap,
+      child: Container(
+        child: child,
+      ),
     );
   }
 }
