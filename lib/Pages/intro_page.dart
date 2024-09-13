d5140fae67e07baec51bfbdfae2700784b3bb173
SolomonB127 <solomonakinsulire@gmail.com>
2024-09-13 13:35:37 +0100

    Created a Pages directory, which contains an intro page.dart as well as every other pages to be used

---
+++ lib/Pages/intro_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class IntroPage extends StatelessWidget {
+  const IntroPage({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Center(
+        child: Icon(
+      Icons.shopping_bag,
+      size: 80.0,
+    ),);
+  }
+}
