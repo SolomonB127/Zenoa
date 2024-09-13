a045614a7b090f0a9004aee59ee28995b5926ba4
SolomonB127 <solomonakinsulire@gmail.com>
2024-09-14 00:38:44 +0100

    Created a themes folder that contains - light theme code - possible dark theme code

---
+++ lib/themes/themes.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+ThemeData lightmode = ThemeData(
+    colorScheme: ColorScheme.light(
+        surface: Colors.grey.shade300,
+        primary: Colors.grey.shade200,
+        secondary: Colors.grey.shade100, inversePrimary: Colors.grey.shade700));
