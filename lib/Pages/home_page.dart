5ad8838e116b6e97e6537de6232cadda9d4a1d2b
SolomonB127 <solomonakinsulire@gmail.com>
2024-09-14 17:44:19 +0100

    feat: Integrated Navigation to CartPage using Drawer.

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
 import 'package:zenoa/Components/drawer_tiles.dart';
 
 class HomePage extends StatelessWidget {
@@ -11,6 +12,12 @@ class HomePage extends StatelessWidget {
         automaticallyImplyLeading: false,
         backgroundColor: Colors.transparent,
         elevation: 0.0,
+        title: Center(
+            child: Text(
+          "Shop Page",
+          style: GoogleFonts.habibi(
+              textStyle: const TextStyle(fontWeight: FontWeight.bold)),
+        )),
         foregroundColor: Theme.of(context).colorScheme.inversePrimary,
         leading: Builder(
             builder: (context) => IconButton(
@@ -31,8 +38,20 @@ class HomePage extends StatelessWidget {
                 const SizedBox(
                   height: 25.0,
                 ),
-                DrawerTiles(icon: Icons.home, text: "Shop", onTap: () {}),
-                DrawerTiles(icon: Icons.trolley, text: "Cart", onTap: () {}),
+                DrawerTiles(
+                    icon: Icons.home,
+                    text: "Shop",
+                    onTap: () => Navigator.pop(context)),
+                DrawerTiles(
+                    icon: Icons.trolley,
+                    text: "Cart",
+                    onTap: () {
+                      // Pop drawer
+                      Navigator.pop(context);
+
+                      // Go to Cart Page
+                      Navigator.pushNamed(context, "/cartpage");
+                    }),
               ],
             ),
             Padding(
