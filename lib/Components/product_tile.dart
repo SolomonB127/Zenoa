cfda2aadf132f7e939e3ae4e53285b526b35215f
SolomonB127 <solomonakinsulire@gmail.com>
2024-09-16 00:25:09 +0100

    Styled up the Product tiles

--- lib/Components/product_tile.dart
+++ lib/Components/product_tile.dart
@@ -8,20 +8,28 @@ class ProductTile extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Column(
-      children: <Widget>[
-        // product image
-        const Icon(Icons.shopify),
+    return Container(
+      decoration: BoxDecoration(
+        color: Theme.of(context).colorScheme.primary,
+        borderRadius: BorderRadius.circular(12),
+      ),
+      margin: const EdgeInsets.all(10),
+      padding: const EdgeInsets.all(25),
+      child: Column(
+        children: <Widget>[
+          // product image
+          const Icon(Icons.shopify),
 
-        // product name
-        Text(product.name),
+          // product name
+          Text(product.name),
 
-        // product description
-        Text(product.description),
+          // product description
+          Text(product.description),
 
-        // product price + add to cart button
-        Text(product.price.toStringAsFixed(2)),
-      ],
+          // product price + add to cart button
+          Text(product.price.toStringAsFixed(2)),
+        ],
+      ),
     );
   }
 }
