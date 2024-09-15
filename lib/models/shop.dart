c50bdfabf7f2e111d8d3b31f647969e36ceec9e4
SolomonB127 <solomonakinsulire@gmail.com>
2024-09-15 16:57:59 +0100

    Created a Shop.dart page, that contains: - Shop class with basic product management functionality

---
+++ lib/models/shop.dart
@@ -0,0 +1,62 @@
+import 'package:zenoa/models/product.dart';
+
+class Shop {
+  // products for sale
+  final List<Product> _shop = [
+    // product 2
+    Product(
+        name: "Product 1",
+        price: 99.99,
+        description: "Item description",
+        imgPath: "imgPath"),
+    // product 2
+    Product(
+        name: "Product 2",
+        price: 99.99,
+        description: "Item description",
+        imgPath: "imgPath"),
+    // product 3
+    Product(
+        name: "Product 3",
+        price: 99.99,
+        description: "Item description",
+        imgPath: "imgPath"),
+    // product 4
+    Product(
+        name: "Product 4",
+        price: 99.99,
+        description: "Item description",
+        imgPath: "imgPath"),
+    // product 5
+    Product(
+        name: "Product 5",
+        price: 99.99,
+        description: "Item description",
+        imgPath: "imgPath"),
+    // product 6
+    Product(
+        name: "Product 6",
+        price: 99.99,
+        description: "Item description",
+        imgPath: "imgPath"),
+  ];
+
+  // user cart
+  final List<Product> _cart = [];
+
+  // get products list
+  List<Product> get shop => _shop;
+
+  // get cart products
+  List<Product> get cart => _cart;
+
+  // add products to cart
+  void addToCart(Product item) {
+    _cart.add(item);
+  }
+
+  // remove products to cart
+  void removeFromCart(Product item) {
+    _cart.remove(item);
+  }
+}
