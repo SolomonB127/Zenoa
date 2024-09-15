e78b34860b68f83d73a5a6ddb8d800e6c52520d1
SolomonB127 <solomonakinsulire@gmail.com>
2024-09-16 00:26:40 +0100

    Mapped through and rendered each individaul products to screen

--- lib/Pages/home_page.dart
+++ lib/Pages/home_page.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
 import 'package:zenoa/Components/drawer_tiles.dart';
+import 'package:zenoa/Components/product_tile.dart';
+import 'package:zenoa/models/shop.dart';
 
 class HomePage extends StatelessWidget {
   const HomePage({super.key});
 
   @override
   Widget build(BuildContext context) {
+    // access products in the shop
+    final products = context.watch<Shop>().shop;
     return Scaffold(
       appBar: AppBar(
         automaticallyImplyLeading: false,
@@ -27,6 +32,32 @@ class HomePage extends StatelessWidget {
                 icon: const Icon(Icons.menu))),
       ),
       backgroundColor: Theme.of(context).colorScheme.surface,
+
+      // List of Products
+      body: ListView(
+        children: <Widget>[
+          // title
+          const Text("SHOP"),
+          // subtitle
+
+          // product list
+          SizedBox(
+            height: 550.0,
+            child: ListView.builder(
+                scrollDirection: Axis.horizontal,
+                padding: const EdgeInsets.all(15),
+                itemCount: products.length,
+                itemBuilder: (context, index) {
+                  // get individual products from shop
+                  final product = products[index];
+
+                  // return as tile UI
+                  return ProductTile(product: product);
+                }),
+          ),
+        ],
+      ),
+
       drawer: Drawer(
         backgroundColor: Theme.of(context).colorScheme.surface,
         child: Column(
