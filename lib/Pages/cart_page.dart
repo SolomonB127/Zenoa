6a69130e74d93de4a4542bf2923542b99846b0af
SolomonB127 <solomonakinsulire@gmail.com>
2024-09-18 12:59:28 +0100

    feat: Pushed items add from homepage into the cart page - Added removal of items in cart page - Added Dialog box for users confirmation on whether on not item should be removed

--- lib/Pages/cart_page.dart
+++ lib/Pages/cart_page.dart
@@ -1,23 +1,73 @@
 import 'package:flutter/material.dart';
 import 'package:google_fonts/google_fonts.dart';
+import 'package:provider/provider.dart';
+import 'package:zenoa/models/product.dart';
+import 'package:zenoa/models/shop.dart';
 
 class CartPage extends StatelessWidget {
   const CartPage({super.key});
 
+  // Remove item from cart
+  void removeItemFromCart(BuildContext context, Product product) {
+    // show dialog box to confirm removal
+    showDialog(
+        context: context,
+        builder: (context) => AlertDialog(
+                content: const Text("Remove from cart?"),
+                actions: <Widget>[
+                  // cancel button
+                  MaterialButton(
+                      onPressed: () => Navigator.pop(context),
+                      child: const Text("Cancel")),
+
+                  // add to cart
+                  MaterialButton(
+                      onPressed: () {
+                        // pop the box
+                        Navigator.pop(context);
+
+                        // Navigate to cart
+                        context.read<Shop>().removeFromCart(product);
+                      },
+                      child: const Text("Yes")),
+                ]));
+  }
+
   @override
   Widget build(BuildContext context) {
+    // getting access to cart
+    final cart = context.watch<Shop>().cart;
     return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Colors.transparent,
-        elevation: 0.0,
-        title: Center(
-          child: Text(
-            "Cart Page",
-            style: GoogleFonts.habibi(),
+        appBar: AppBar(
+          backgroundColor: Colors.transparent,
+          elevation: 0.0,
+          title: Center(
+            child: Text(
+              "Cart Page",
+              style: GoogleFonts.habibi(),
+            ),
           ),
         ),
-      ),
-      backgroundColor: Theme.of(context).colorScheme.surface,
-    );
+        backgroundColor: Theme.of(context).colorScheme.surface,
+        body: Column(children: <Widget>[
+          // cart list
+          Expanded(
+              child: ListView.builder(
+                  itemCount: cart.length,
+                  itemBuilder: (context, index) {
+                    // get individual items
+                    final item = cart[index];
+                    // return as cart list
+                    return ListTile(
+                      title: Text(item.name),
+                      subtitle: Text(item.price.toStringAsFixed(2)),
+                      trailing: IconButton(
+                          onPressed: () => removeItemFromCart(context, item),
+                          icon: const Icon(Icons.delete)),
+                    );
+                  }))
+
+          // pay btn
+        ]));
   }
 }
